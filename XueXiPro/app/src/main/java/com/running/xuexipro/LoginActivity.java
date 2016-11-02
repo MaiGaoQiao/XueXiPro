23f72a166064c36ddc947062e6e31767ed70978a
Running John <running727@gmail.com>
2016-11-02 20:38:46 +0800

    b

--- XueXiPro/app/src/main/java/com/running/xuexipro/LoginActivity.java
+++ XueXiPro/app/src/main/java/com/running/xuexipro/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.running.xuexipro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+    }
+
+
+    public void login(View v){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        intent.putExtra("type", FeiMiApp.NewsContentType_DiMingZhi);
+        startActivity(intent);
+        finish();
     }
 }
